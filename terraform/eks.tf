e590e8119e3887b42a7b8c6a97b683e303d9a0c0
tejaravutla287 <tejaravutla287@gmail.com>
2026-07-20 15:52:40 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,21 +1,19 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
-
   cluster_name    = "prime-poc-cluster"
   cluster_version = "1.30"
-
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
-
   cluster_endpoint_public_access = true
 
+  # Strict single-node instance rule
   eks_managed_node_groups = {
     monitoring_node = {
       min_size       = 1
       max_size       = 1
       desired_size   = 1
-      instance_types = ["c7i-flex.large"] # Fits application + ArgoCD + Helm Monitoring
+      instance_types = ["c7i-flex.large"]
     }
   }
 }
