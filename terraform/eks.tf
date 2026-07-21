bb79bbc23f32a226d99f46fda8f26f3f139a57cb
tejaravutla287 <tejaravutla287@gmail.com>
2026-07-21 12:58:09 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,19 +1,32 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
+
   cluster_name    = "prime-poc-cluster"
   cluster_version = "1.30"
+
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
-  cluster_endpoint_public_access = true
 
-  # Strict single-node instance rule
+  cluster_endpoint_public_access = true
+  enable_cluster_creator_admin_permissions = true
+
   eks_managed_node_groups = {
     monitoring_node = {
       min_size       = 1
       max_size       = 1
       desired_size   = 1
-      instance_types = ["c7i-flex.large"]
+      instance_types = ["c7i-flex.large"] # 2 vCPUs, 4 GiB RAM
+
+      # Unique naming strategy to resolve the Degraded AccessDenied bug
+      iam_role_use_name_prefix = true
+      iam_role_name            = "eks-node-poc-unique-role"
+      
+      iam_role_additional_policies = {
+        AmazonEKSWorkerNodePolicy          = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+        AmazonEKS_CNI_Policy               = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+        AmazonEC2ContainerRegistryReadOnly = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+      }
     }
   }
 }
